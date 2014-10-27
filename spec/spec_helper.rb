f0f36c89bf7ccc16afd27b459f048369219af300
ChaYoung You <yousbe@gmail.com>
2014-10-27 19:04:04 +0900

    Add task `coverage`

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
 # encoding: utf-8
 
+if ENV['COVERAGE']
+  require 'simplecov'
+
+  SimpleCov.start do
+    add_filter '/spec/'
+  end
+end
+
 require 'sawarineko'
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
