99d16a4fa9f2361b7477c733423186d9346db5f3
ChaYoung You <yousbe@gmail.com>
2014-10-29 00:43:03 +0900

    Make test to describe `Converter#convert`

--- spec/sawarineko/converter_spec.rb
+++ spec/sawarineko/converter_spec.rb
@@ -5,42 +5,44 @@ require 'spec_helper'
 RSpec.describe Sawarineko::Converter do
   subject(:converter) { described_class.new }
 
-  it 'converts hiragana な to にゃ' do
-    expect(converter.convert('な')).to eq('にゃ')
-    expect(converter.convert('ななめななじゅうななどのならびで' \
-                             'なくなくいななくななはんななだい' \
-                             'なんなくならべてながながめ'))
-      .to eq('にゃにゃめにゃにゃじゅうにゃにゃどのにゃらびで' \
-             'にゃくにゃくいにゃにゃくにゃにゃはんにゃにゃだい' \
-             'にゃんにゃくにゃらべてにゃがにゃがめ')
-  end
-
-  it 'converts katakana ナ to ニャ' do
-    expect(converter.convert('ナ')).to eq('ニャ')
-    expect(converter.convert('ナナメナナジュウナナドノナラビデ' \
-                             'ナクナクイナナクナナハンナナダイ' \
-                             'ナンナクナラベテナガナガメ'))
-      .to eq('ニャニャメニャニャジュウニャニャドノニャラビデ' \
-             'ニャクニャクイニャニャクニャニャハンニャニャダイ' \
-             'ニャンニャクニャラベテニャガニャガメ')
-  end
-
-  it 'converts hangul 나-낳 to 냐-냫' do
-    ('나'..'낳').zip('냐'..'냫').each do |na, nya|
-      expect(converter.convert(na)).to eq(nya)
+  describe '#convert' do
+    it 'converts hiragana な to にゃ' do
+      expect(converter.convert('な')).to eq('にゃ')
+      expect(converter.convert('ななめななじゅうななどのならびで' \
+                               'なくなくいななくななはんななだい' \
+                               'なんなくならべてながながめ'))
+        .to eq('にゃにゃめにゃにゃじゅうにゃにゃどのにゃらびで' \
+               'にゃくにゃくいにゃにゃくにゃにゃはんにゃにゃだい' \
+               'にゃんにゃくにゃらべてにゃがにゃがめ')
     end
-    expect(converter.convert('나랑 너랑 봄나들이 배낭 매고 봄나들이 ' \
-                             '버드나무 낭창낭창 남실바람 남실남실 ' \
-                             '개나리 꽃에 나비가 하나 ' \
-                             '배낭 속에 바나나가 하나'))
-      .to eq('냐랑 너랑 봄냐들이 배냥 매고 봄냐들이 ' \
-             '버드냐무 냥창냥창 냠실바람 냠실냠실 ' \
-             '개냐리 꽃에 냐비가 하냐 ' \
-             '배냥 속에 바냐냐가 하냐')
-  end
 
-  it 'converts all languages at once' do
-    expect(converter.convert(%w(な ナ 나).join("\n")))
-      .to eq(%w(にゃ ニャ 냐).join("\n"))
+    it 'converts katakana ナ to ニャ' do
+      expect(converter.convert('ナ')).to eq('ニャ')
+      expect(converter.convert('ナナメナナジュウナナドノナラビデ' \
+                               'ナクナクイナナクナナハンナナダイ' \
+                               'ナンナクナラベテナガナガメ'))
+        .to eq('ニャニャメニャニャジュウニャニャドノニャラビデ' \
+               'ニャクニャクイニャニャクニャニャハンニャニャダイ' \
+               'ニャンニャクニャラベテニャガニャガメ')
+    end
+
+    it 'converts hangul 나-낳 to 냐-냫' do
+      ('나'..'낳').zip('냐'..'냫').each do |na, nya|
+        expect(converter.convert(na)).to eq(nya)
+      end
+      expect(converter.convert('나랑 너랑 봄나들이 배낭 매고 봄나들이 ' \
+                               '버드나무 낭창낭창 남실바람 남실남실 ' \
+                               '개나리 꽃에 나비가 하나 ' \
+                               '배낭 속에 바나나가 하나'))
+        .to eq('냐랑 너랑 봄냐들이 배냥 매고 봄냐들이 ' \
+               '버드냐무 냥창냥창 냠실바람 냠실냠실 ' \
+               '개냐리 꽃에 냐비가 하냐 ' \
+               '배냥 속에 바냐냐가 하냐')
+    end
+
+    it 'converts all languages at once' do
+      expect(converter.convert(%w(な ナ 나).join("\n")))
+        .to eq(%w(にゃ ニャ 냐).join("\n"))
+    end
   end
 end
